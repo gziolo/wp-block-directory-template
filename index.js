c4274f5887d46968d6f70cd84feaf2d25ba8dcfc
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-11-04 12:33:04 +0100

    Add minimal npm setup for the project

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {
 	defaultValues: {
-		slug: 'block-directory-example',  
+		slug: 'block-directory-example',
 		namespace: 'gziolo',
 		title: 'Block Directory Example',
-		description: 'Example block written with ESNext standard and JSX support for publishing to Block Directory.',
-    
+		description:
+			'Example block written with ESNext standard and JSX support for publishing to Block Directory.',
+
 		dashicon: 'palmtree',
 	},
 	templatesPath: __dirname,
