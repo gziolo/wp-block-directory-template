b335cc8b7be3368f407a7568d1caefa002d6a26a
Greg Ziółkowski <grzegorz@gziolo.pl>
2020-11-04 12:05:40 +0100

    Add main entry file with the template configuration

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	defaultValues: {
+		slug: 'block-directory-example',  
+		namespace: 'gziolo',
+		title: 'Block Directory Example',
+		description: 'Example block written with ESNext standard and JSX support for publishing to Block Directory.',
+    
+		dashicon: 'palmtree',
+	},
+	templatesPath: __dirname,
+};
